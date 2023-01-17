a648fc9f6070fe316f7cc1b3068e7b49a4e74d65
YareYareOjiSan <113853299+YareYareOjiSan@users.noreply.github.com>
2023-01-17 22:19:16 +0000

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,61 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('ananas'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'img/jsonfiles/ananas.json'
+})
+
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('apple'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'img/jsonfiles/apple.json'
+})
+
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('banana'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'img/jsonfiles/banana.json'
+})
+
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('grape'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'img/jsonfiles/grape.json'
+})
+
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('orange'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'img/jsonfiles/orange.json'
+})
+
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('pom'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'img/jsonfiles/pom.json'
+})
+
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('strawberry'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'img/jsonfiles/strawberry.json'
+})
